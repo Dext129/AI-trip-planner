6b596f6896f204d6c89d23e1996a19020eb67456
Dext129 <129tanish@gmail.com>
2025-10-12 15:22:35 +0530

    added popular destination

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Hero from "./_components/Hero";
+import { PopularCityList } from "./PopularCityList";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <PopularCityList />
     </div>
   );
 }
+  
